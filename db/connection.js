8034170d1b50c1d37020013de8691fb87ca35b34
Julian EMERY <julian.emery@epitech.eu>
2023-04-12 19:03:09 +0200

    [ADD] Routes to create asset, agency and user

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,6 @@
 import { MongoClient } from "mongodb";
+import GridFsStorage from 'multer-gridfs-storage';
+import Grid from 'gridfs-stream';
 
 const connectionString = process.env.MONGO_URL || "";
 
@@ -7,6 +9,10 @@ const client = new MongoClient(connectionString);
 let conn;
 try {
     conn = await client.connect();
+    conn.once('open', () => {
+        gfs = Grid(conn.db);
+        gfs.collection('uploads');
+  });
 } catch(e) {
     console.error(e);
 }
--- routes.js
+++ routes.js
@@ -1,6 +1,24 @@
 import express from "express";
 import db from "./db/connection.js";
+import multer from "multer";
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname)
+    }
+});
 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({storage: storage, limits: {fileSize: 100 * 1024 * 1024}, fileFilter: fileFilter})
 const router = express.Router();
 
 // Get a list of 50 posts
@@ -11,6 +29,42 @@ router.get("/", async (req, res) => {
     .toArray();
 
   res.send(results).status(200);
+});
+
+router.post("/agency", async (req, res) => {
+    let collection = await db.collection("agency");
+    try {
+        var newAgency = {name : req.body.name, logo: req.body.logo};
+    } catch (err) {
+        res.status(400);
+    }
+    let result = await collection.insertOne(newAgency);
+    res.send(result).status(204);
+});
+
+router.post("/asset", upload.array("images", 10), async (req, res) => {
+    await console.log(req.file);
+    res.status(200);
+    // let collection = await db.collection("asset");
+    // try {
+    //     console.log(req.body)
+    //     var newAsset = {title: req.body.title, address: req.body.address, number_of_rooms: req.body.number_of_rooms, images: req.body.images}
+    // } catch (err) {
+    //     res.status(400);
+    // }
+    // let result = await collection.insertOne(newAsset);
+    // res.send(result).status(204);
+});
+
+router.post("/user", async (req, res) => {
+    let collection = await db.collection("user");
+    try {
+        var newUser = {email: req.body.email, password: req.body.password, user_type: req.body.user_type}
+    } catch (err) {
+        res.status(400);
+    }
+    let result = await collection.insertOne(newUser);
+    res.send(result).status(204);
 });
 
 export default router;
